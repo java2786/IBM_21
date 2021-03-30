1231c3e9e540fe7197d3d4d9a5bb99256746514f
Arun Kumar <java.kumar.arun@gmail.com>
2021-03-30 13:01:34 +0530

    Angular basics, module, component

--- 21.Angular/angular-demo/src/main.ts
+++ 21.Angular/angular-demo/src/main.ts
@@ -1,7 +1,37 @@
-import { enableProdMode } from '@angular/core';
+import { Component, enableProdMode, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+@Component({
+  selector: "my-first-component",
+  template: `<h1>It is my very first angular component+++</h1>`,
+  styles: []
+})
+class AppComponent{
+  constructor(){
+    console.log("my first component is rendered")
+  }
+}
+
+// import { AppModule } from './app/app.module';
+// @NgModule({
+//   declarations: [component, pipe, ],
+//   providers: [reusable code, util, service, guards],
+//   imports: [others modules],
+//   exports: [..],  
+//   bootstrap: [Component]
+// })
+@NgModule({
+  declarations: [AppComponent],
+  imports:[
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+class AppModule{}
+
+
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
