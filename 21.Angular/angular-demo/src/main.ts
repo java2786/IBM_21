ae560451956de8e2543ef0f0b34543e71c428f01
Arun Kumar <java.kumar.arun@gmail.com>
2021-04-01 10:56:55 +0530

    hightlight + unless

--- 21.Angular/angular-demo/src/main.ts
+++ 21.Angular/angular-demo/src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './06_badges/app.module';
 import { environment } from './environments/environment';
 
 // import AppModule from "./app/app.module";
@@ -11,6 +10,8 @@ import { environment } from './environments/environment';
 // import {AppModule} from "./04_parent-child/app.module";
 // import {AppModule} from "./05_directives/app.module";
 // import AppModule from './practice_01/app.module';
+// import { AppModule } from './06_badges/app.module';
+import { AppModule } from './07_directives/app.module';
 
 
 
