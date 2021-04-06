0a22c1d4a0409930337350702bec1b7f944ab34e
Arun Kumar <java.kumar.arun@gmail.com>
2021-04-06 15:08:32 +0530

    angular + testing

--- 22.Angular_Testing/angular-testing/src/app/app.module.ts
+++ 22.Angular_Testing/angular-testing/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
+import { Demo2Component } from './demo2/demo2.component';
+import { DemoDemoComponent } from './demo-demo/demo-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    CapitalizePipe,
+    Demo2Component,
+    DemoDemoComponent
   ],
   imports: [
     BrowserModule,
