62089c986561245c18698a1116f5708bcd78bfa5
Arun Kumar <java.kumar.arun@gmail.com>
2021-04-07 17:21:05 +0530

    reactjs + function vs class component + props + defaultProps+style+click

--- 25. React/react-app/src/App.js
+++ 25. React/react-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Badge from './components/badge'
 
-function App() {
+/* 
+class App extends React.Component {
+  myFunc(){
+
+  }
+  render(){
+    return (
+      <div>
+        <h1>My application</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe fugiat enim architecto nesciunt vero deserunt libero assumenda totam, perspiciatis exercitationem numquam aliquid, itaque ullam quod! Mollitia saepe dolore hic. Ad.</p>
+      </div>
+    );
+  }
+}
+ */
+
+
+const App = () => {
+  const sendNotifications = () => {
+    console.log("sendig notification");
+  }
+  const showSent = () => {
+    console.log("show sent messages");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My application</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe fugiat enim architecto nesciunt vero deserunt libero assumenda totam, perspiciatis exercitationem numquam aliquid, itaque ullam quod! Mollitia saepe dolore hic. Ad.</p>
+      <Badge myMethod={sendNotifications} text="Notification" color="black" backgroundColor="darkseagreen" count="42" /><br />
+      <Badge myMethod={showSent} text="Sent" count="15" />
     </div>
   );
 }
+
 
 export default App;
